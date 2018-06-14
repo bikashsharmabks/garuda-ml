93fcdc0cbc0aa4d69b24b208837e11c42bf14ed5
Bikash Sharma <bikash1999@yahoo.com>
2018-06-14 21:19:28 +0530

    added model

--- ml/user-topic.py
+++ ml/user-topic.py
@@ -11,7 +11,7 @@ import os
 tf.enable_eager_execution()
 
 
-# In[87]:
+# In[90]:
 
 
 from sklearn import preprocessing, cross_validation
@@ -25,8 +25,8 @@ COLUMN_NAMES = ['screen_name','name','description', 'class']
 CLASSES = ['sports','technology','entertainment', 'politics',
            'music','legal','medical','education','journalism']
 
-num_labels = len(CLASSES)
-VOCAB_SIZE = 1000
+NUM_CLASSES = 4 #len(CLASSES)
+MAX_VOCABULARY_SIZE = 1000
 BATCH_SIZE = 100
 EPOCHS = 5
 
@@ -46,7 +46,7 @@ def raw_data_fn(y_name='class'):
     classes = data['class']
 
     # define Tokenizer with Vocab Size
-    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=VOCAB_SIZE)
+    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=MAX_VOCABULARY_SIZE)
     tokenizer.fit_on_texts(name_description)
     
     # define encoder and make labels
@@ -70,6 +70,24 @@ def train_input_fn(features, labels, batch_size):
     dataset = dataset.shuffle(1000).repeat().batch(batch_size)
     
     return dataset
+
+def model_fn():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Dense(512, activation=tf.nn.relu, input_shape=(MAX_VOCABULARY_SIZE,)))
+    model.add(tf.keras.layers.Dense(NUM_CLASSES, activation=tf.nn.softmax))
+
+    # Create a TensorFlow optimizer, rather than using the Keras version
+    # This is currently necessary when working in eager mode
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=0.001)
+
+    # We will now compile and print out a summary of our model
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=optimizer,
+                  metrics=['accuracy'])
+
+    model.summary()
+    return model
+    
     
 features, labels = raw_data_fn()
 #print(features[0])
@@ -79,6 +97,12 @@ train_features, test_features, train_labels, test_labels = load_data_fn(features
 print("Train count {features/labels}: %s/%s" % (len(train_features), len(train_labels)))
 print("Test count {features/labels}: %s/%s" % (len(test_features), len(test_labels)))
 
-train_dataset = train_input_fn(train_features, train_labels, BATCH_SIZE)
-print(train_dataset)
+#train_dataset = train_input_fn(train_features, train_labels, BATCH_SIZE)
+#print(train_dataset)
+
+model = model_fn()
+model.fit(train_features, train_labels, epochs=EPOCHS)
+loss, accuracy = model.evaluate(test_features, test_labels)
+print(loss)
+print(accuracy)
 
