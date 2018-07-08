ff6e29a2802a6f5ced99850904aa6244c5449f77
Shruti Tanwar <shruti.tanwar93@gmail.com>
2018-07-08 23:40:39 +0530

    added docker file

--- dashboard/src/views/Home/Home.js
+++ dashboard/src/views/Home/Home.js
@@ -1,15 +1,62 @@
 import React, {Component} from 'react';
 // import { Link } from 'react-router'; import superagent from 'superagent';
 // import moment from 'moment';
+import superagent from 'superagent';
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      formName:'gender classification',
+      value: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleGenderClick = this.handleGenderClick.bind(this);
+    this.handleTextClick = this.handleTextClick.bind(this);
+    this.handleImageClick = this.handleImageClick.bind(this);
+    this.getGender = this.getGender.bind(this);
+    this.getTopic = this.getTopic.bind(this);
   }
 
-  handleChange() {
-    console.log("Change")
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  getGender(event) {
+    console.log(this.state.value)
+    superagent.get('api/predictions/gender')
+      .send({ name: this.state.value})
+      .set('Accept', 'application/json')
+      .end((error, response) => {
+        if (error) {
+          console.log(error)
+        } else {
+          let res = response.body;
+          console.log(res)
+        }
+      });
+  }
+
+  getTopic(event){
+
+  }
+
+  handleGenderClick(){
+    this.setState({
+      formName :'gender classification'
+    });
+  }
+
+  handleTextClick(){
+    this.setState({
+      formName :'text classification'
+    });
+  }
+
+  handleImageClick(){
+    this.setState({
+      formName :'image classification'
+    });
   }
 
   componentDidMount() {}
@@ -38,7 +85,7 @@ class Home extends Component {
                     </a>
                   </div>
                   <div className="text-center">
-                    <button type="button" className="btn btn-primary btn-primary-custom">Try this API</button>
+                    <button type="button" className="btn btn-primary btn-primary-custom" onClick={this.handleGenderClick}>Try this API</button>
                   </div>
                 </div>
               </div>
@@ -51,7 +98,7 @@ class Home extends Component {
                     </a>
                   </div>
                   <div className="text-center">
-                    <button type="button" className="btn btn-primary btn-primary-custom">Try this API</button>
+                    <button type="button" className="btn btn-primary btn-primary-custom" onClick={this.handleTextClick}>Try this API</button>
                   </div>
                 </div>
               </div>
@@ -64,7 +111,7 @@ class Home extends Component {
                     </a>
                   </div>
                   <div className="text-center">
-                    <button type="button" className="btn btn-primary btn-primary-custom">Try this API</button>
+                    <button type="button" className="btn btn-primary btn-primary-custom" onClick={this.handleImageClick}>Try this API</button>
                   </div>
                 </div>
               </div>
@@ -74,12 +121,20 @@ class Home extends Component {
         <div className="container text-center text-container">
           <div className="row">
             <div className="col-sm-12">
-              <form>
+              {this.state.formName === "gender classification" ?
+              (<form onSubmit={this.getGender}>
                 <h2 style={{"color": "dimgray"}}>Gender Classification </h2>
                 <label></label>
-                <input className="custom-text text-center" type="text" placeholder="Enter a name" value={this.state.inputvalue} onChange={this.handleChange}/>
-                <a href="#" type="button" className="btn-floating button-floating-custom2">Go!</a>
-              </form>
+                <input className="custom-text text-center" type="text" placeholder="Enter a name" value={this.state.value} onChange={this.handleChange}/>
+                <input type="submit" value="Go" />
+              </form>) : ""}
+              {this.state.formName === "text classification" ?
+              (<form onSubmit={this.getTopic}>
+                <h2 style={{"color": "dimgray"}}>Gender Classification </h2>
+                <label></label>
+                <input className="custom-text text-center" type="text" placeholder="Enter a name" value={this.state.value} onChange={this.handleChange}/>
+                <input type="submit" value="Go" />
+              </form>) : ""}
             </div>
           </div>
         </div>
