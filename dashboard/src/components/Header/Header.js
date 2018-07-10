512a665873ce0b6a74e92fa7f6f8e8aaabb26527
Shruti <shruti.tanwar93@gmail.com>
2018-07-10 18:00:13 +0530

    docker doen for dashboard

--- dashboard/src/components/Header/Header.js
+++ dashboard/src/components/Header/Header.js
@@ -22,10 +22,9 @@ class Header extends Component {
       <header className="app-header navbar">
         <a className="navbar-brand navbar-brand-custom" href="#"></a>
         <div className="nav navbar nav-item hidden-md-down">
-          <div className="h4 mt-auto" style={{'color':'dimgray'}}>People10 ML-AI API's</div>
+          <div className="h4 mt-auto" style={{ 'color': 'dimgray' }}>People10 ML-AI API's</div>
         </div>
         <div className="nav navbar-nav hidden-md-down">
-        
         </div>
       </header>
     )
