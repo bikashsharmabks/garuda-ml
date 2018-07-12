82cb3eb1a786199549a1053adf840935f205edeb
AashishDahiya <aashish.d@people10.com>
2018-07-12 15:41:17 +0530

    api modified.

--- gender-classification/serve.py
+++ gender-classification/serve.py
@@ -23,11 +23,13 @@ def predict_gender():
 		res = pred.predict_gender(name);
 		result = list(res);
 		if(result[0] == 'female'):
-			result[1] = 100.0 - result[1];
+			result[1] = str(round(100.0 - round(result[1]*100,2)*100,2)) + "%";
+		else:
+			result[1] = str(round(result[1]*100,2)) + " %"	
 		response = Response(response=json.dumps({
         	"name": name,
         	"gender":result[0],
-        	"probability": str(round(result[1]*100,2)) + " %"
+        	"probability": result[1];
         }),
         status=200,
         mimetype='application/json'
